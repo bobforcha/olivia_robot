4a917dc41026f16a6641d8ed20fa9c5e8104ee6a
bobforcha <bobforcha@gmail.com>
2020-06-30 00:39:43 -0400

    start python controller layout

---
+++ python/test_window.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from Tkinter import *
+
+def sel():
+    selection = "Speed = " + str(speed.get())
+    label.config(text = selection)
+
+root = Tk()
+root.geometry("960x540")
+speed = IntVar()
+angle = IntVar()
+speedScale = Scale(root, variable = speed, from_ = 0, to = 255, length = 300, label = "Speed")
+speedScale.pack(anchor = W)
+
+turnScale = Scale(root, variable = angle, from_ = -127, to = 127, length = 300, orient = HORIZONTAL, label = "Turn")
+turnScale.pack(anchor = CENTER)
+
+button = Button(root, text = "Get Scale Value", command = sel)
+button.pack(anchor = CENTER)
+
+label = Label(root)
+label.pack()
+
+root.mainloop()
